ae6a37754ee01ddc851aa74fb1d2bd12ad55457c
Kimdoun <52628184+Kimdoun@users.noreply.github.com>
2019-07-07 23:21:18 +0900

    Create HashKey.java

---
+++ IoT-BlockChain/Android/HashKey.java
@@ -0,0 +1,27 @@
+package com.example.kimdoun.iot;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class HashKey extends StringRequest{
+    final  static  private String URL = "http://172.30.98.123:80/hash.php";
+    private Hashtable<String, String> parameters;
+    public HashKey(String Num, String key, String Command, String Prkey, Response.Listener<String> listener)
+    {
+        // hashMap 을 이용해서 데이터 DB로 전송.
+        super(Method.POST, URL,listener,null);
+        parameters = new Hashtable<>();
+        parameters.put("Num",Num);
+        parameters.put("key",key);
+        parameters.put("Command",Command);
+        parameters.put("Prkey",Prkey);
+    }
+    @Override
+    public Hashtable<String, String> getParams() {
+        return parameters;
+    }
+}
