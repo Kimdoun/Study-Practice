4bb6441292214fd292b9d39823063202d6acb0d0
Kimdoun <52628184+Kimdoun@users.noreply.github.com>
2019-07-07 23:21:24 +0900

    Create SHA256.java

---
+++ IoT-BlockChain/Android/SHA256.java
@@ -0,0 +1,27 @@
+package com.example.kimdoun.iot;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class SHA256 {
+    public static String Num="";
+    public static int Cnt;
+    public static String SHA256(String Number,String Command, String str){
+        String SHA="";
+        str=Number+Command+str;
+        try{
+            MessageDigest sh = MessageDigest.getInstance("SHA-256");
+            sh.update(str.getBytes());
+            byte byteData[] = sh.digest();
+            StringBuffer sb = new StringBuffer();
+            for(int i = 0 ; i < byteData.length; i++){
+                sb.append(Integer.toString((byteData[i]&0xff)+0x100, 16).substring(1));
+            }
+            SHA = sb.toString();
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            SHA = null;
+        }
+        return SHA;
+    }
+}
